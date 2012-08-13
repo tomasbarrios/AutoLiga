2b23fe7890e1090b7a7a5e4002614f9ffa62e6d4
Tomás Barrios <tomasbarrios@gmail.com>
2012-08-13 02:39:15 -0400

    Carrusel para auspiciadores

--- system/application/views/layout/footer.php
+++ system/application/views/layout/footer.php
@@ -1,1 +1,10 @@
+ <ul id="banner-sponsors">
+  <?php foreach($sponsors_banner as $sponsor) :?>
+    <li>
+      <a href="<?php echo $sponsor->url?>">
+        <img src="<?php echo imagen_sponsor($sponsor->id_sponsor)?>" width="100px" alt="<?php echo $sponsor->nombre?>" />
+      </a>
+    </li>
+  <?php endforeach; ?>
+  </ul>
 <!--Sitio web desarrollado por <a href="mailto:tomasbarrios@gmail.com">Silla de Playa</a> Software Factory - 2010.--> 
